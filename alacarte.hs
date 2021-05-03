213dfc95f035dc046617553924ddefcc5b2f157e
dany <ifeifei@stu.xmu.edu.cn>
2021-05-03 19:18:38 +0800

    add an parse tree for the memory operations

--- alacarte.hs
+++ alacarte.hs
@@ -34,6 +34,12 @@ import Combinator
 import Components
 import TypeLogic
 
+newtype Mem = Mem Int deriving (Show)
+mapMem :: (Int -> Int) -> Mem -> Mem
+mapMem f (Mem x) = Mem (f x)
+runMem :: Mem -> Int
+runMem (Mem m) = m
+
 data Incr t = Incr Int t
 data Recall t = Recall (Int -> t)
 data Clear t = Clear t
@@ -41,6 +47,12 @@ data Clear t = Clear t
 deriving instance Functor Incr
 deriving instance Functor Recall
 deriving instance Functor Clear
+instance (Show t) => Show (Incr t) where
+    show (Incr a t) = "Show Incr " ++ show a ++ show t
+instance (Show t) => Show (Recall t) where
+    show (Recall r) = "Show Recall Int -> " ++ show (r 1)
+instance (Show t) => Show (Clear t) where
+    show (Clear t) = "Show Clear " ++ show t
 
 class (Functor sub,Functor sup) => sub :<: sup where
     inj :: sub a -> sup a
@@ -50,6 +62,10 @@ instance (Functor f, Functor g) => f :<: (f :+: g) where
     inj = L1
 instance (Functor f, Functor g, Functor h, f :<: g) => f :<: (h :+: g) where
     inj = R1 . inj
+
+type MemOp =  Incr :+: Recall :+: Clear
+instance Show (Free MemOp ()) where
+    show a = "Free memop ()"
 
 inject :: (g :<: f) => g (Free f a) -> Free f a
 inject = Free . inj
@@ -63,18 +79,105 @@ recall = inject (Recall return)
 clear :: (Clear :<: f) => Free f ()
 clear = inject (Clear (return ()))
 
-type MemOp =  Incr :+: Recall :+: Clear
 
-
--- op = incr i op | recall (t -> op) | clear op
--- memop :: Parser (Free MemOp a)
--- memop = do {string "clear"; return clear }
---         +++ do { string "recall"; return recall }
---         +++ do { _ <- string "incr"; x <- number; return $ incr x}
+-- newtype StateMem a = StateMem (Mem -> (a, Mem)) deriving (Functor)
+-- data MS f t = MS (Int -> Free f (Int -> MS f t))
+-- type FreeMS f t = Free f (MS f Int)
 --
--- number :: Parser Int
--- number = do { ns <- many1 digit1; return $ foldl (\x y-> x*10 + y) 0 ns}
--- digit1 = do { x <- token (sat isDigit); return $  (ord x - ord '0')}
+-- incr :: (Incr :<: f) => FreeMS f t -> Int -> FreeMS f t
+-- incr t i = inject (Incr i t)
+--
+-- recall :: (Recall :<: f) => FreeMS f t -> FreeMS f t
+-- recall t = inject (Recall Pure)
+--
+-- clear :: (Clear :<: f) => Free f () -> Free f ()
+-- clear t = inject (Clear (return ()))
+--
+
+input :: String
+input = "recall; incr 1; clear; incr 1; incr 1; recall; end"
+
+data IncrExpr e = IncrExpr Int e
+data RecallExpr e = RecallExpr e
+data ClearExpr e = ClearExpr e
+data EndExpr e = EndExpr
+deriving instance Functor IncrExpr
+deriving instance Functor RecallExpr
+deriving instance Functor ClearExpr
+deriving instance Functor EndExpr
+instance (Show t) => Show (IncrExpr t) where
+    show (IncrExpr a t) = "Show Incr " ++ show a ++ show t
+instance (Show t) => Show (RecallExpr t) where
+    show (RecallExpr t) = "Show Recall " ++ show t
+instance (Show t) => Show (ClearExpr t) where
+    show (ClearExpr t) = "Show Clear " ++ show t
+instance (Show t) => Show (EndExpr t) where
+    show (EndExpr) = "Show End"
+-- instance (Show (f a), Show (g a))  => Show ( (f :+: g) a) where
+--     show (L1 f) = show f
+--     show (R1 g) = show g
+
+
+data Expr f = In (f (Expr f))
+instance Show (Expr CompExpr) where
+    show (In t) = show t
+type CompExpr = EndExpr :+: IncrExpr :+: RecallExpr :+: ClearExpr
+injectExpr :: (g :<: f) => g (Expr f) -> Expr f
+injectExpr = In . inj
+incrExpr :: (IncrExpr :<: f) => Int -> Expr f -> Expr f
+incrExpr i e = injectExpr $ IncrExpr i e
+recallExpr :: (RecallExpr :<: f) => Expr f -> Expr f
+recallExpr e = injectExpr $ RecallExpr e
+clearExpr :: (ClearExpr :<: f) => Expr f -> Expr f
+clearExpr e  = injectExpr $ ClearExpr e
+endExpr :: (EndExpr :<: f) => Expr f
+endExpr = injectExpr $ EndExpr
+
+memop :: Parser (Expr (EndExpr :+: IncrExpr :+: RecallExpr :+: ClearExpr))
+memop = do { symb "clear"; symb ";"; t <- memop; return $ clearExpr t } +++
+        do { symb "recall"; symb ";"; t <- memop; return $ recallExpr t } +++
+        do { symb "incr"; x <- number; symb ";"; t <- memop; return $ incrExpr x t } +++
+        do { symb "end"; return $ endExpr }
+
+number :: Parser Int
+number = do { ns <- many1 digit1; return $ foldl (\x y-> x*10 + y) 0 ns}
+digit1 = do { x <- token (sat isDigit); return $  (ord x - ord '0')}
+
+foldExpr :: Functor f => (f a -> a) -> Expr f -> a
+foldExpr f (In t) = f (fmap (foldExpr f ) t)
+
+class Functor f => Run f where
+    runAlgebra :: (Run f0) => f (Expr f0) ->  Free MemOp (Either () Int)
+instance Run IncrExpr where
+    runAlgebra (IncrExpr i (In t)) = do { incr i; runAlgebra t}
+    -- runAlgebra (IncrExpr i (In t)) = trace "incr_expr" $ do { incr i; runAlgebra t}
+instance Run RecallExpr where
+    runAlgebra (RecallExpr (In t)) = do { x <- recall; runAlgebra t; return $ Right x}
+    -- runAlgebra (RecallExpr (In t)) = trace "recall_expr" $ do { x <- recall; runAlgebra t; return ()}
+instance Run ClearExpr where
+    runAlgebra (ClearExpr (In t)) = do { clear; runAlgebra t }
+    -- runAlgebra (ClearExpr (In t)) = trace "clear_expr" $ do { clear; runAlgebra t }
+instance Run EndExpr where
+    runAlgebra (EndExpr) = return $ Left ()
+    -- runAlgebra (EndExpr) = trace "end_expr" $ return ()
+instance (Run f, Run g) => Run (f :+: g) where
+    runAlgebra (L1 r) = runAlgebra r
+    runAlgebra (R1 r) = runAlgebra r
+    -- runAlgebra (L1 r) = trace "L" $ runAlgebra r
+    -- runAlgebra (R1 r) = trace "R" $ runAlgebra r
+
+exprx = fst $ (parse memop input)!!0
+expr :: CompExpr (Expr CompExpr)
+expr = let In t = exprx in t
+
+checkExpr :: Expr CompExpr -> Bool
+checkExpr (In (L1 EndExpr)) = False
+checkExpr _ = True
+
+whenM :: (Monoid a, Monad m) => Bool -> m a -> m a
+whenM x m = if x then m else return mempty
+whileM :: (Monoid a, Monad m) => m Bool -> m a -> m a
+whileM act m = do {b <- act; whenM b m}
 
 
 tick :: Free MemOp Int     -- Pure () | Incr 1 (Free Incr ()) :: Free Incr ()
@@ -84,15 +187,24 @@ tick = do
         incr 1
         incr 1
         return y
-
+tickz :: Free MemOp Int     -- Pure () | Incr 1 (Free Incr ()) :: Free Incr ()
+tickz = do
+        y <- recall
+        clear
+        z <- recall
+        incr 1
+        incr 2
+        x <- recall
+        return y
+        return x
+tickv :: Free MemOp ()
+tickv = do
+        clear
+        incr 1
+        incr 2
 -- peekNum :: Free MemOp Int
 -- peekNum = do {y <- recall; return y}
 
-newtype Mem = Mem Int deriving (Show)
-mapMem :: (Int -> Int) -> Mem -> Mem
-mapMem f (Mem x) = Mem (f x)
-runMem :: Mem -> Int
-runMem (Mem m) = m
 
 newtype CoIncr a = CoIncr (Int -> a) deriving (Functor)
 newtype CoRecall a = CoRecall (Int, a) deriving (Functor)
@@ -166,81 +278,31 @@ instance (Functor f, Functor g, PairPred f g ~ flag, Pairing' flag f g) => Pairi
 instance {-# OVERLAPPABLE #-} (Functor f, Functor g) => Pairing' HFalse f g where
     pair' _ _ _ _ = error "No pair method"
 instance {-# OVERLAPs #-} Pairing' HTrue Identity Identity where
-    pair' _ f (Identity a) (Identity b) = trace "Pairing Identity Identity" f a b
+    pair' _ f (Identity a) (Identity b) = f a b
+    -- pair' _ f (Identity a) (Identity b) = trace "Pairing Identity Identity" f a b
 instance {-# OVERLAPs #-} Pairing' HTrue ((->) a) ((,) a) where
-    pair' _ p f = trace "Pairing ((->) a) ((,) a)" uncurry (p . f)
+    pair' _ p f = uncurry (p . f)
+    -- pair' _ p f = trace "Pairing ((->) a) ((,) a)" uncurry (p . f)
 instance {-# OVERLAPs #-} Pairing' HTrue ((,) a) ((->) a) where
-    pair' _ p f g =  trace " Pairing ((,) a) ((->) a)" pair (flip p) g f
+    pair' _ p f g = pair (flip p) g f
+    -- pair' _ p f g =  trace " Pairing ((,) a) ((->) a)" pair (flip p) g f
 instance {-# OVERLAPs #-} Pairing f g => Pairing' HTrue (Cofree f) (Free g) where
-    pair' _ p (a :< _ ) (Pure x)  = trace "CF PURE" p a x
-    pair' _ p (_ :< fs) (Free gs) = trace "CF FREE" pair (pair p) fs gs
+    pair' _ p (a :< _ ) (Pure x)  = p a x
+    pair' _ p (_ :< fs) (Free gs) = pair (pair p) fs gs
+    -- pair' _ p (a :< _ ) (Pure x)  = trace "CF PURE" p a x
+    -- pair' _ p (_ :< fs) (Free gs) = trace "CF FREE" pair (pair p) fs gs
 instance {-# OVERLAPs #-} (Pairing g f, Pairing g h) => Pairing' HTrue g (f :+: h) where
-    pair' _ p g (L1 f) = trace "Pairing g ([f] :+: g)" $ pair p g f
-    pair' _ p g (R1 h) = trace "Pairing g (f :+: [g])" $ pair p g h
+    pair' _ p g (L1 f) = pair p g f
+    pair' _ p g (R1 h) = pair p g h
+    -- pair' _ p g (L1 f) = trace "Pairing g ([f] :+: g)" $ pair p g f
+    -- pair' _ p g (R1 h) = trace "Pairing g (f :+: [g])" $ pair p g h
 instance {-# OVERLAPs #-} (Functor h, Or (CastHTrue (PairPred f g)) (PairPred h g) ~ HLTrue, Pairing f g) => Pairing' HLTrue (f :*: h) g where
-    pair' _ p (f :*: h) g = trace "Pairing ([f] :*: h) g" $ pair p f g
+    pair' _ p (f :*: h) g = pair p f g
+    -- pair' _ p (f :*: h) g = trace "Pairing ([f] :*: h) g" $ pair p f g
 instance {-# OVERLAPs #-} (Functor f, Or (PairPred f g) (CastHTrue (PairPred h g)) ~ HRTrue, Pairing h g) => Pairing' HRTrue (f :*: h) g where
-    pair' _ p (f :*: h) g = trace "Pairing (f :*: [h]) g" $ pair p h g
+    pair' _ p (f :*: h) g = pair p h g
+    -- pair' _ p (f :*: h) g = trace "Pairing (f :*: [h]) g" $ pair p h g
 
--- class (Functor f, Functor g) => Pairing f g where
---     pair :: (a -> b -> r) -> f a -> g b -> r
---     cp :: Int -> f a -> g b -> Bool
---     cp i _ _ = trace (show i ++ ": default true") True
---
--- instance {-# OVERLAPPABLE #-} (Functor f, Functor g) => Pairing f g where
---     pair _ fa gb = trace ("default instance " ) error "No such paring "
---     cp i _ _ = trace (show i ++ ": default instance cp") False
--- instance {-# OVERLAPs #-} Pairing Identity Identity where
---     pair f (Identity a) (Identity b) = trace "Pairing Identity Identity" f a b
--- instance {-# OVERLAPs #-} Pairing ((->) a) ((,) a) where
---     pair p f = trace "Pairing ((->) a) ((,) a)" uncurry (p . f)
--- instance {-# OVERLAPs #-} Pairing ((,) a) ((->) a) where
---     pair p f g =  trace " Pairing ((->) a) ((,) a)" pair (flip p) g f
--- instance {-# OVERLAPs #-} Pairing f g => Pairing (Cofree f) (Free g) where
---     pair p (a :< _ ) (Pure x)  = trace "CF PURE" p a x
---     pair p (_ :< fs) (Free gs) = trace "CF FREE" pair (pair p) fs gs
---     cp i (a :< _ ) (Pure x) =  trace (show i ++ ": Error! ") error "text"
---     cp i (_ :< fs) (Free gs) = trace (show i ++ ": cp CF = " ++ show (cp 0 fs gs)) cp (i+1) fs gs
-
--- instance (Pairing g f, Pairing k h) => Pairing (g :*: k) (f :+: h) where
---   -- pair p (g :*: _) (L1 f) = pair p g f
---   -- pair p (_ :*: k) (R1 h) = pair p k h
---   -- cp (g :*: _) (L1 f) = cp g f
---   -- cp (_ :*: k) (R1 h) = cp k h
--- instance {-# OVERLAPs #-} (Functor f, Functor h, Functor g) => Pairing g (f :+: h) where
---     pair p g (L1 f) = trace "Pairing g ([f] :+: g)" $ if cp 0 g f then pair p g f else error "No such paring "
---     pair p g (R1 h) = trace "Pairing g (f :+: [g])" $ if cp 0 g h then pair p g h else error "No such paring "
---     cp i g (L1 f) = cp (i+1) g f
---     cp i g (R1 h) = cp (i+1) g h
---
--- instance {-# OVERLAPs #-} (Functor f, Functor g, Functor k) => Pairing (g :*: k) f where
---     pair p (g :*: k) f = trace "Pairing (g :*: k) f" $ if cp 0 g f then trace "select left intp" pair p g f else trace "select right intp" pair p k f
---     cp i x f = case x of g :*: k ->  trace (show i ++ ": cp g*k f = " ++ show (cp 0 g f) ++ show (cp 0 k f)) $ (cp (i+1) k f) || (cp (i+2) g f)
-
--- instance {-# OVERLAPs #-} (Pairing g f, Functor k) => Pairing (g :*: k) f where
---     pair p (g :*: k) f = trace "Pairing (g :*: k) f" $ if cp 0 g f then trace "select left intp" pair p g f else trace "select right intp" pair p k f
---     cp i (g :*: k) f = trace (show i ++ ": cp g*k f = " ++ show (cp 0 g f) ++ show (cp 0 k f)) $ (cp (i+1) k f) || (cp (i+2) g f)
--- instance {-# OVERLAPs #-} (Pairing k f, Functor g) => Pairing (g :*: k) f where
---     pair p (g :*: k) f = trace "Pairing (g :*: k) f" $ if cp 0 g f then trace "select left intp" pair p g f else trace "select right intp" pair p k f
---     cp i (g :*: k) f = trace (show i ++ ": cp g*k f = " ++ show (cp 0 g f) ++ show (cp 0 k f)) $ (cp (i+1) k f) || (cp (i+2) g f)
---
--- instance {-# OVERLAPs #-} (Functor f, Functor g, Functor h, Functor k) => Pairing (g :*: k) (f :+: h) where
---   pair p (g :*: k) (L1 f) = trace "Pairing (g :*: k) ([f] :+: h)" $ if cp 0 g f then trace "pgf" pair p g f else trace "pkf" pair p k f
---   pair p k (R1 h) = trace "Pairing (g :*: k) (f :+: [h])" $ pair p k h
---   cp i g (L1 f) = cp (i+1) g f
---   cp i g (R1 h) = cp (i+1) g h
-
--- instance (Pairing g f, h :<: g, Functor k) => Pairing (k :*: h) f where
---     pair p (_ :*: h) f = pair p (inj h) f
--- -- instance (Pairing g f, Functor h) =>  Pairing (h :*: g) f where
--- --     pair p (_ :*: g) f = pair p g f
--- instance (Pairing g f, Pairing k h) => Pairing (g :*: k) (f :+: h) where
---       pair p (g :*: _) (L1 f) = pair p g f
---       pair p (_ :*: k) (R1 h) = pair p k h
-
--- instance (Pairing g f, f :<: (h :+: k), (m :*: n) :<: g) => Pairing (m :*: n) (h :+: k) where
---     pair p coprod prod = pair p (inj coprod) (inj prod)
---     pair p coprod (R1 f) = pair p (inj coprod) f
 
 instance {-# OVERLAPs #-} Pairing' HTrue CoIncr Incr where
     pair' _ f (CoIncr i) (Incr x t) = f (i x) t
@@ -249,44 +311,56 @@ instance {-# OVERLAPs #-} Pairing' HTrue CoRecall Recall where
 instance {-# OVERLAPs #-} Pairing' HTrue CoClear Clear where
     pair' _ f (CoClear a) (Clear b) = f a b
 
-foldTerm :: Functor f => (a -> b) -> (f b -> b) -> Free f a -> b
-foldTerm pur imp (Pure x) = pur x
-foldTerm pur imp (Free t) = imp (fmap (foldTerm pur imp) t)
-
-class Functor f => Run f where
-    runAlgebra :: f (Mem -> (a, Mem)) -> (Mem -> (a, Mem))
-
-instance Run Incr where
-    runAlgebra (Incr k r) (Mem i) = r (Mem (i+k))
-instance Run Recall where
-    runAlgebra (Recall r) (Mem i) = r i (Mem i)
-instance Run Clear where
-    runAlgebra (Clear r) (Mem i) = r (Mem 0)
-instance (Run f, Run g) => Run (f :+: g) where
-    runAlgebra (L1 r) = runAlgebra r
-    runAlgebra (R1 r) = runAlgebra r
-
-run :: Run f => Free f a -> Mem -> (a, Mem)
-run = foldTerm (,) runAlgebra
-
-
+-- foldTerm :: Functor f => (a -> b) -> (f b -> b) -> Free f a -> b
+-- foldTerm pur imp (Pure x) = pur x
+-- foldTerm pur imp (Free t) = imp (fmap (foldTerm pur imp) t)
+--
+-- class Functor f => Run f where
+--     runAlgebra :: f (Mem -> (a, Mem)) -> (Mem -> (a, Mem))
+--
+-- instance Run Incr where
+--     runAlgebra (Incr k r) (Mem i) = r (Mem (i+k))
+-- instance Run Recall where
+--     runAlgebra (Recall r) (Mem i) = r i (Mem i)
+-- instance Run Clear where
+--     runAlgebra (Clear r) (Mem i) = r (Mem 0)
+-- instance (Run f, Run g) => Run (f :+: g) where
+--     runAlgebra (L1 r) = runAlgebra r
+--     runAlgebra (R1 r) = runAlgebra r
+--
+-- run :: Run f => Free f a -> Mem -> (a, Mem)
+-- run = foldTerm (,) runAlgebra
 
 
-s = Mem 0
+--
+-- t :: Free Recall Int
+-- t = recall >>= (\y -> return y)
+--     do {y <- recall; return y}
+
+s = Mem 10
 c = (CoIncr $ coIncr s)
 i = (CoIncr $ coIncr s) :*: (CoRecall $ coRecall s) :*: (CoClear $ coClear s)
 lInt = Identity 1 :*: Identity 2
 rInt = Identity 3
 t = Incr 1 1
 
+-- get the memory value
+m = pair const (interpretMem s) tick
+mzv = pair const (interpretMem s) tickz
+-- get the recall value
+r = pair (flip const) (interpretMem s) tick
+mzr = pair (flip const) (interpretMem s) tickz
 
+mvv = pair const (interpretMem s) tickv
+mvr = pair (flip const) (interpretMem s) tickv
 
+e =  runAlgebra expr
+mev =  pair const (interpretMem s) e
+mer = pair (flip const) (interpretMem s) e
 
-
-
-
-
-
+re x = case x of
+        Pure _ -> "Pure"
+        Free _ -> "Free"
 
 
 
